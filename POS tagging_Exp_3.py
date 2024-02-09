30f4f6c7c5682d4c06201486bb4492326ba657e0
Shivanshu Chauhan <89507255+razputshivanshu@users.noreply.github.com>
2024-02-09 13:13:17 +0530

    Add files via upload

---
+++ POS tagging_Exp_3.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+import nltk
+
+text = word_tokenize("DSA, which stands for Data Structures and Algorithms, is a fundamental concept in computer science. It encompasses the study and implementation of various data structures (such as arrays, linked lists, trees, graphs, etc.) and algorithms (such as searching, sorting, traversal, etc.) that enable efficient data organization, manipulation, and retrieval.")
+nltk.pos_tag(text)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
